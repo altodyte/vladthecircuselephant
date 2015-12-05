15bb627d7950b03193ae5fe32e29f8682f609d8c
Michael Searing <msearing@olin.edu>
2015-12-05 16:54:42 -0500

    Made constants settable over serial, and cleaned up code

--- proportionalController/proportionalController.ino
+++ proportionalController/proportionalController.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 #include <Encoder.h>
 #include <Wire.h> // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation is used in I2Cdev.h
 #include <Adafruit_MotorShield.h>
@@ -20,15 +21,9 @@ Encoder enc2(18,16);
 Encoder enc3(19,17);
 //   avoid using pins with LEDs attached
 
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-
-// Adafruit_DCMotor *motor0 = AFMS.getMotor(1);
-// Adafruit_DCMotor *motor1 = AFMS.getMotor(2);
-// Adafruit_DCMotor *motor2 = AFMS.getMotor(3);
-// Adafruit_DCMotor *motor3 = AFMS.getMotor(4);
-
-HardwareSerial & motorSer = Serial;
-HardwareSerial & proSer = Serial3;
+// HardwareSerial & compSer = Serial;
+HardwareSerial & sysSer = Serial3;
+// #define DEBUG
 char inChar;
 char mOutNames[] = {'a', 'b', 'c', 'd'};
 int mOutVals[4];
@@ -61,48 +56,55 @@ int deadZone = 2;
 int loopDuration = 2; // loop should last as close to 2 milliseconds as possible
 long long nextLoop = 0;
 
-double roll = 0, pitch = 0;
-double rollError = 0, pitchError = 0;
 
-double constantA = 333.333;
-double constantB = 332.333;
-double constantC = 0.9998;
-double k_P = 1;
+const unsigned char numC = 5;
+double constants[numC];
+const char inChars[] = {'a', 'b', 'c', 'k', 'p'};
 
-double pitchVp = 0;
-double pitchVpLast = pitchVp;
-double pitchErrorLast = pitchError;
 double pitchPsi = 0;
+double pitchPsiSet = 0;
+double pitchPsiError = 0;
+double pitchPhi = 0;
+double pitchPhiError = 0;
+double pitchVp = 0;
+double pitchVpLast = 0;
+double pitchPhiErrorLast = 0;
 
-double rollVp = 0;
-double rollVpLast = rollVp;
-double rollErrorLast = rollError;
 double rollPsi = 0;
+double rollPsiSet = 0;
+double rollPsiError = 0;
+double rollPhi = 0;
+double rollPhiError = 0;
+double rollVp = 0;
+double rollVpLast = 0;
+double rollPhiErrorLast = 0;
 
 
 void setup() {
-  proSer.setTimeout(5);
+  sysSer.setTimeout(5);
 
   // initialize serial communication
   Serial.begin(250000);
-  proSer.begin(250000);
+  sysSer.begin(250000);
+  fuck(); // kills motors
 
   // configure Arduino LED for heartbeat
   pinMode(LED_PIN, OUTPUT);
 
-  // Initialize motor shield
-  // Serial.println("Initializing AFMS I2C device...");
-  // AFMS.begin();
-  fuck(); // kills motors
+  for (k = 0; k < numC; ++k) constants[k] = EEPROM_readDouble(4*k);
 
   nextLoop = millis() + loopDuration;
 }
 
-// long long startTime = 0;
+#ifdef DEBUG
+  long long startTime = 0;
+#endif
 
 void loop() {
   if (millis() > nextLoop) {
-    // startTime = micros();
+    #ifdef DEBUG
+      startTime = micros();
+    #endif
 
     nextLoop += loopDuration;
 
@@ -111,59 +113,55 @@ void loop() {
     digitalWrite(LED_PIN, blinkState);
 
     // takes 5.5 ms for accelerometer to get new data
-    while (proSer.available() > 0) {
-      inChar = proSer.read();
-      if (inChar == 'r') roll = proSer.parseFloat();
-      else if (inChar == 'p') pitch = proSer.parseFloat();
+    while (sysSer.available()) {
+      inChar = sysSer.read();
+      if (inChar == 'r') rollPhi = sysSer.parseFloat();
+      else if (inChar == 'p') pitchPhi = sysSer.parseFloat();
+    }
+
+    // read constants from serial
+    while (Serial.available()) {
+      inChar = Serial.read();
+      for (k = 0; k < numC; ++k) if (inChar == inChars[k]) EEPROM_writeDouble(k*4, constants[k] = Serial.parseFloat());
+      // if (inChar == 'd') {
+      //   for (k = 0; k < numC; ++k) { Serial.print(constants[k]); Serial.print(" "); }
+      //   Serial.println();
+      // }
     }
 
     // Serial.print(roll, 5);
     // Serial.print(' ');
     // Serial.println(pitch, 5);
 
-    // proSer.print(roll, 5);
-    // proSer.print(' ');
-    // proSer.println(pitch, 5);
+    // calculate phi errors
+    rollPhiError = 0 - rollPhi;
+    pitchPhiError = 0 - pitchPhi;
 
-    // calculate roll and pitch errors
-    rollError = 0 - roll;
-    pitchError = 0 - pitch;
-
-    // pitchPsi = ticksToRadians(long((-enc1.read()+enc3.read())/2));
-    // voltageIntoPitchPositionFeedback = constantC*previousVoltageIntoPitchPositionFeedback + constantA*pitchError + constantB*previousPitchError;
-    // pitchActuators = voltageToMotorShield(voltageIntoPitchPositionFeedback + kayPee*pitchPsi);
-    // previousPitchError = pitchError;
-    // previousVoltageIntoPitchPositionFeedback = voltageIntoPitchPositionFeedback;
-    // m1set(pitchActuators);
-    // m3set(-pitchActuators);
-
-    // rollPsi = ticksToRadians(long((-enc0.read()+enc2.read())/2));
-    // voltageIntoRollPositionFeedback = constantC*previousVoltageIntoRollPositionFeedback + constantA*rollError + constantB*previousRollError;
-    // rollActuators = voltageToMotorShield(voltageIntoRollPositionFeedback + kayPee*rollPsi);
-    // previousRollError = rollError;
-    // previousVoltageIntoRollPositionFeedback = voltageIntoRollPositionFeedback;
-    // m0set(-rollActuators);
-    // m2set(rollActuators);
-
+    // calculate psi and psi errors
     rollPsi = ticksToRadians(long((-enc0.read()-enc2.read())/2));
-    rollVp = constantC*rollVpLast - constantA*rollError + constantB*rollErrorLast;
-    // rollActuators = voltageToMotorShield(rollVp + k_P*rollPsi);
-    rollActuators = 500.0*roll; // p control
-    rollErrorLast = rollError;
+    rollPsiError = rollPsi - rollPsiSet;
+    pitchPsi = ticksToRadians(long((enc1.read()+enc3.read())/2));
+    pitchPsiError = pitchPsi - pitchPsiSet;
+
+    // calculate control signals
+    // rollVp = constants[2]*rollVpLast - constants[0]*rollPhiError + constants[1]*rollPhiErrorLast;
+    // rollVv = rollVp + constants[3]*rollPsiError;
+    // rollVa = voltageToMotorShield(rollVv);
+    rollActuators = constants[4]*rollPhi; // p control
+    // pitchVp = constants[2]*pitchVpLast - constants[0]*pitchPhiError + constants[1]*pitchPhiErrorLast;
+    // pitchVv = pitchVp + constants[3]*pitchPsiError;
+    // pitchVa = voltageToMotorShield(pitchVv);
+    pitchActuators = constants[4]*pitchPhi; // p control
+
+    // save current values for next loop
+    rollPhiErrorLast = rollPhiError;
     rollVpLast = rollVp;
-    // m0set(-rollActuators);
-    // m2set(rollActuators);
+    pitchPhiErrorLast = pitchPhiError;
+    pitchVpLast = pitchVp;
+
+    // set motor control value array, doing sign conversion for motor orientation correction
     mOutVals[0] = -rollActuators;
     mOutVals[2] = rollActuators;
-
-    pitchPsi = ticksToRadians(long((enc1.read()+enc3.read())/2));
-    pitchVp = constantC*pitchVpLast - constantA*pitchError + constantB*pitchErrorLast;
-    // pitchActuators = voltageToMotorShield(pitchVp + k_P*pitchPsi);
-    pitchActuators = 500.0*pitch; // p control
-    pitchErrorLast = pitchError;
-    pitchVpLast = pitchVp;
-    // m1set(pitchActuators);
-    // m3set(-pitchActuators);
     mOutVals[1] = pitchActuators;
     mOutVals[3] = -pitchActuators;
 
@@ -179,52 +177,24 @@ void loop() {
     // proSer.print(' ');
     // proSer.println(pitchActuators);
     
+    // send motor commands every other loop, because motor controller is too slow to take commands every loop
     if (++k % 2 == 0) {
       for (j = 0; j < 4; ++j) {
-        motorSer.print(mOutNames[j]);
-        motorSer.print(mOutVals[j]);
+        sysSer.print(mOutNames[j]);
+        sysSer.print(mOutVals[j]);
       }
     }
 
-    // proSer.println((long) (micros() - startTime));
-
-    // pitchActuators = modify(proportionalConstant*pitchError);
-    // m1set(-pitchActuators);
-    // m3set(pitchActuators);
-
-    // rollActuators = modify(proportionalConstant*rollError);
-    // m0set(rollActuators);
-    // m2set(-rollActuators);
-
-    // Serial.print("Pitch: ");
-    // Serial.print(pitch);
-    // Serial.print(" || ");
-    // Serial.print("M2: ");
-    // Serial.print(-pitchActuators);
-    // Serial.print(" | ");
-    // Serial.print("M4: ");
-    // Serial.print(pitchActuators);
-    // Serial.print(" ||| ");
-    // Serial.print("Roll: ");
-    // Serial.print(roll);
-    // Serial.print(" || ");
-    // Serial.print("M1: ");
-    // Serial.print(rollActuators);
-    // Serial.print(" | ");
-    // Serial.print("M3: ");
-    // Serial.println(-rollActuators);
+    #ifdef DEBUG
+      Serial.println((long) (micros() - startTime));
+    #endif
   }
 }
 
 void fuck(){
-    // Serial.println("KILL MOTORS");
-    // m0set(0);
-    // m1set(0);
-    // m2set(0);
-    // m3set(0);
     for (j = 0; j < 4; ++j) {
-      motorSer.print(mOutNames[j]);
-      motorSer.print(0);
+      sysSer.print(mOutNames[j]);
+      sysSer.print(0);
     }
 }
 
@@ -282,10 +252,6 @@ int modify(int in){
   }
 }
 
-// int voltageToMotorShield(double voltage){
-//     return int(scale(coerce(voltage, -12.0, 12.0),-12.0,12.0,-255.0,255.0));
-// }
-
 long long voltageToMotorShield(double voltage){
     return (long long) 255*(voltage/12.0);
 }
@@ -298,58 +264,18 @@ double d2r(double d){
   return d*M_PI/180.0;
 }
 
-/******************************************************************************
-***************************** Motor Driving Functions *************************
-******************************************************************************/
+void EEPROM_writeDouble(int ee, double value)
+{
+   byte* p = (byte*)(void*)&value;
+   for (int i = 0; i < sizeof(value); i++)
+       EEPROM.write(ee++, *p++);
+}
 
-// void m0set(int i){
-//   if (i==0) {
-//     motor0->run(RELEASE);
-//     motor0->setSpeed(0);
-//   } else if (i<0){
-//     motor0->setSpeed(abs(i));
-//     motor0->run(BACKWARD);
-//   } else {
-//     motor0->setSpeed(i);
-//     motor0->run(FORWARD);
-//   }
-// }
-
-// void m1set(int i){
-//   if (i==0) {
-//     motor1->run(RELEASE);
-//     motor1->setSpeed(0);
-//   } else if (i<0){
-//     motor1->setSpeed(abs(i));
-//     motor1->run(BACKWARD);
-//   } else {
-//     motor1->setSpeed(i);
-//     motor1->run(FORWARD);
-//   }
-// }
-
-// void m2set(int i){
-//   if (i==0) {
-//     motor2->run(RELEASE);
-//     motor2->setSpeed(0);
-//   } else if (i<0){
-//     motor2->setSpeed(abs(i));
-//     motor2->run(BACKWARD);
-//   } else {
-//     motor2->setSpeed(i);
-//     motor2->run(FORWARD);
-//   }
-// }
-
-// void m3set(int i){
-//   if (i==0) {
-//     motor3->run(RELEASE);
-//     motor3->setSpeed(0);
-//   } else if (i<0){
-//     motor3->setSpeed(abs(i));
-//     motor3->run(BACKWARD);
-//   } else {
-//     motor3->setSpeed(i);
-//     motor3->run(FORWARD);
-//   }
-// }
+double EEPROM_readDouble(int ee)
+{
+   double value = 0.0;
+   byte* p = (byte*)(void*)&value;
+   for (int i = 0; i < sizeof(value); i++)
+       *p++ = EEPROM.read(ee++);
+   return value;
+}
