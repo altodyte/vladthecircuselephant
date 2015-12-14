81efd70f40d27defce025f582fba5b2bcbf639a4
Michael Searing <msearing@olin.edu>
2015-12-13 20:11:17 -0500

    changed some more things

--- RF24testUNO/RF24testUNO.ino
+++ RF24testUNO/RF24testUNO.ino
@@ -2,150 +2,88 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 
-//
-// Hardware configuration
-//
+// Become the primary transmitter (ping out)
+#define TX true
 
-// Set up nRF24L01 radio on SPI bus plus pins 9 & 10 
-
-RF24 radio(9,10);
-
-//
-// Topology
-//
+// Set up nRF24L01 radio on SPI bus plus chip enable (9) & slave select (UNO:10; MEGA:53)
+RF24 radio(9,SS);
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
-// The various roles supported by this sketch
-typedef enum { role_ping_out = 1, role_pong_back } role_e;
-
-// Become the primary transmitter (ping out)
-role_e role = role_ping_out;
-
-// Become the primary receiver (pong back)
-// role_e role = role_pong_back;
-
-void setup(void)
-{
-  //
-  // Print preamble
-  //
-
-  Serial.begin(57600);
-
-  //
-  // Setup and configure rf radio
-  //
+void setup(void) {
+  Serial.begin(250000);
 
   radio.begin();
 
-  // optionally, increase the delay between retries & # of retries
-  radio.setRetries(15,15);
-
-  // optionally, reduce the payload size.  seems to
-  // improve reliability
-  //radio.setPayloadSize(8);
-
-  //
-  // Open pipes to other nodes for communication
-  //
-
-  // This simple sketch opens two pipes for these two nodes to communicate back and forth.
-  // Open 'our' pipe for writing
-  // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-
-  if ( role == role_ping_out ) {
+  #if TX
     radio.openWritingPipe(pipes[0]);
     radio.openReadingPipe(1,pipes[1]);
-  } else {
+  #else
     radio.openWritingPipe(pipes[1]);
     radio.openReadingPipe(1,pipes[0]);
-  }
-
-  //
-  // Start listening
-  //
+  #endif
 
   radio.startListening();
-
-  //
-  // Dump the configuration of the rf unit for debugging
-  //
-
-  // radio.printDetails();
 }
 
-void loop(void)
-{
-  //
-  // Ping out role.  Repeatedly send the current time
-  //
-
-  if (role == role_ping_out)
-  {
+void loop(void) {
+  #if TX
     // First, stop listening so we can talk.
     radio.stopListening();
 
     // Take the time, and send it.  This will block until complete
-    unsigned long time = millis();
-    Serial.print("Now sending "); Serial.println(time);
-    bool ok = radio.write( &time, sizeof(unsigned long) );
+    float time = millis();
+    Serial.print("Now sending "); Serial.println(time/1.0);
+    bool ok = radio.write( &time, sizeof(float) );
 
     // Now, continue listening
     radio.startListening();
 
     // Wait here until we get a response, or timeout (250ms)
-    unsigned long started_waiting_at = millis();
+    float started_waiting_at = millis();
     bool timeout = false;
-    while ( ! radio.available() && ! timeout )
+    while ( !radio.available() && !timeout )
       if (millis() - started_waiting_at > 200)
         timeout = true;
 
     // Describe the results
     if ( timeout )
-      Serial.println("Response timed outh");
+      Serial.println("Response timed out");
     else {
       // Grab the response, compare, and send to debugging spew
-      unsigned long got_time;
-      radio.read( &got_time, sizeof(unsigned long) );
+      float got_time;
+      radio.read( &got_time, sizeof(float) );
       Serial.print("Got response "); Serial.println(got_time);
     }
 
     // Try again 1s later
-    delay(1000);
-  }
-
-  //
-  // Pong back role.  Receive each packet, dump it out, and send it back
-  //
-
-  if ( role == role_pong_back )
-  {
+    // delay(100);
+  #else
     // if there is data ready
     if ( radio.available() )
     {
       // Dump the payloads until we've gotten everything
-      unsigned long got_time;
+      float got_time;
       bool done = false;
       while (!done)
       {
         // Fetch the payload, and see if this was the last one.
-        done = radio.read( &got_time, sizeof(unsigned long) );
+        done = radio.read( &got_time, sizeof(float) );
         Serial.print("Got payload "); Serial.println(got_time);
 
-      	// Delay just a little bit to let the other unit make the transition to receiver
-      	delay(20);
+        // Delay just a little bit to let the other unit make the transition to receiver
+        delay(20);
       }
       // First, stop listening so we can talk
       radio.stopListening();
 
       // Send the final one back.
-      radio.write( &got_time, sizeof(unsigned long) );
+      radio.write( &got_time, sizeof(float) );
       Serial.println("Sent response");
 
       // Now, resume listening so we catch the next packets.
       radio.startListening();
     }
-  }
+  #endif
 }
